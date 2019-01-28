40ab2f14b63500e103c3a9e65ddf59bb5c1b8283
arleyribeiro <arley.sribeiro@gmail.com>
2019-01-28 16:29:53 -0200

    Adicionando classe posiçao

---
+++ C#_Completo_2019/xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ C#_Completo_2019/xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,18 @@
+namespace tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString() {
+            return $"Posição: {Linha}, {Coluna}";
+        }
+    }
+}
