b181d26d24d206d2b0fd0f520a8b603234ef2b41
arleyribeiro <arley.sribeiro@gmail.com>
2019-08-03 15:54:44 -0300

    video 22

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,9 +8,11 @@ import { Component } from "@angular/core";
 
 export class PostListComponent {
 
-    posts = [ 
+    /* posts = [ 
         { title: "First Post", content: "This is the first post's content" },
         { title: "Second Post", content: "This is the second post's content" },
         { title: "Third Post", content: "This is the third post's content" }
-    ];
+    ]; */
+
+    posts = [];
 }
