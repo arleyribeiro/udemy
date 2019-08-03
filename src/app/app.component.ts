be6ae37b1b31fa7a0b24a75a48693487dd5be66e
arleyribeiro <arley.sribeiro@gmail.com>
2019-08-03 16:10:49 -0300

    video 23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mean-course';
+  storedPosts = [];
+
+  onPostAdded(post) {
+      this.storedPosts.push(post);
+  }
 }
