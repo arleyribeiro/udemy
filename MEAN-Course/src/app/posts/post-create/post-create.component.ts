2b68ce0837582ad3d7e7ca7c3a0e27e77d8d6c2a
arleyribeiro <arley.sribeiro@gmail.com>
2019-08-03 19:04:21 -0300

    29 - Working on our form

--- MEAN-Course/src/app/posts/post-create/post-create.component.ts
+++ MEAN-Course/src/app/posts/post-create/post-create.component.ts
@@ -19,5 +19,6 @@ export class PostCreateComponent {
         if(form.invalid)
             return;
         this.postsService.addPost(form.value.title, form.value.content);
+        form.resetForm();
     }
 }
